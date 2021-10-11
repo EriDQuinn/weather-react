46abf735eb1ff1545fd258f16a5b7007dec683cf
ErikaDiaz <edaeri@live.com.mx>
2021-10-10 23:29:09 -0500

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchWeather from "./SearchWeather.js";
+import Forecast from "./Forecast.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Enter a City to search the weather</h2>
+        <SearchWeather></SearchWeather>
       </header>
+      <Forecast></Forecast>
+
+      <p>
+        <a href="">Open-source code,</a>by <a href="">Erika D.</a>{" "}
+      </p>
     </div>
   );
 }
